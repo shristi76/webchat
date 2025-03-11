65da80eeeefebbe23b826381c4d0224da3c02bca
shri599 <shristishri426@gmail.com>
2025-03-11 20:57:22 +0530

    first

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins (change this in production)
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("sendMessage", (data) => {
+    io.emit("receiveMessage", data); // Broadcast message to all users
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Real-time Chat Server is Running...");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
